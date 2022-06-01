79096b4219a32f96b3a9c33f2b0d6ad7bf390892
dede rusliandi <hello.derusta@gmail.com>
2022-06-01 09:57:03 +0700

    add package passport & uuid

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         $this->call(UsersTableSeeder::class);
+        
+        \Artisan::call('passport:install');
     }
 }
